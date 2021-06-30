00a25129b7f50f5dc87d52bca4523669f1e2e258
Lori A. Burns <lori.burns@gmail.com>
2021-06-30 11:30:53 -0400

    mp3, ccd, lccd, lccsd harvesting. ref hessians.

--- qcengine/programs/nwchem/runner.py
+++ qcengine/programs/nwchem/runner.py
@@ -248,17 +248,30 @@ task python
         #   in the parsing of output files
         qcvars, nwhess, nwgrad, nwmol, version, module, errorTMP = harvest(input_model.molecule, stdout, **outfiles)
 
-        if nwgrad is not None:
-            qcvars[f"{input_model.model.method.upper()[4:]} TOTAL GRADIENT"] = nwgrad
-            qcvars["CURRENT GRADIENT"] = nwgrad
-        if nwhess is not None:
-            qcvars["CURRENT HESSIAN"] = nwhess
+        try:
+            if nwgrad is not None:
+                qcvars[f"{input_model.model.method.upper()[4:]} TOTAL GRADIENT"] = nwgrad
+                qcvars["CURRENT GRADIENT"] = nwgrad
 
-        # Normalize the output as a float or list of floats
-        if input_model.driver.upper() == "PROPERTIES":
-            retres = qcvars[f"CURRENT ENERGY"]
-        else:
-            retres = qcvars[f"CURRENT {input_model.driver.upper()}"]
+            if nwhess is not None:
+                qcvars[f"{input_model.model.method.upper()[4:]} TOTAL HESSIAN"] = nwhess
+                qcvars["CURRENT HESSIAN"] = nwhess
+
+            # Normalize the output as a float or list of floats
+            if input_model.driver.upper() == "PROPERTIES":
+                retres = qcvars[f"CURRENT ENERGY"]
+            else:
+                retres = qcvars[f"CURRENT {input_model.driver.upper()}"]
+        except KeyError as e:
+            raise UnknownError(
+                "STDOUT:\n"
+                + stdout
+                + "\nSTDERR:\n"
+                + stderr
+                + "\nTRACEBACK:\n"
+                + "".join(traceback.format_exception(*sys.exc_info()))
+            )
+
         if isinstance(retres, Decimal):
             retres = float(retres)
         elif isinstance(retres, np.ndarray):
